63a771b29127cd62681fdab87d8080f18d8e1133
noro16 <149973833+noro16@users.noreply.github.com>
2024-02-17 20:09:27 +0100

    Update buildJar.groovy

--- vars/buildJar.groovy
+++ vars/buildJar.groovy
@@ -1,6 +1,10 @@
 #!/usr/bin/env groovy
 
-def call() {
-    echo "building the application for branch $BRANCH_NAME"
-    sh 'mvn package'
+def buildImage() {
+    echo "building the docker image..."
+    withCredentials([usernamePassword(credentialsId: 'dockers', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh 'docker build -t noro16/my-repo:jma-2.0 .'
+        sh "echo $PASS | docker login -u $USER --password-stdin"
+        sh 'docker push noro16/my-repo:jma-2.0'
+    }
 }
