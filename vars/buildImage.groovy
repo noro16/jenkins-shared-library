e14bd743a130c3c1302a08d7c1817b7e057f8292
noro16 <149973833+noro16@users.noreply.github.com>
2024-02-17 20:40:03 +0100

    Update buildImage.groovy

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,10 +1,10 @@
 #!/usr/bin/env groovy
 
-def buildImage() {
+def call(String imageName) {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'dockers', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t noro16/my-repo:jma-2.0 .'
+        sh 'docker build -t $imageName .'
         sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push noro16/my-repo:jma-2.0'
+        sh "docker push $imageName"
     }
 }
