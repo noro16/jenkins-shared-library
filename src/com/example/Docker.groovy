31aa010f9d7cc0373347763414a0906fa136cdc5
noro16 <149973833+noro16@users.noreply.github.com>
2024-03-10 17:35:45 +0100

    Update Docker.groovy

--- src/com/example/Docker.groovy
+++ src/com/example/Docker.groovy
@@ -14,11 +14,7 @@ class Docker implements Serializable {
         script.sh "docker build -t $imageName ."
     }
    def dockerLogin() {
-        script.withCredentials([script.usernamePassword(credentialsId: 'dockers', passwordVariable: 'PASS', usernameVariable: 'USER')])
-                {
-            script.sh "echo $script.PASS | docker login -u $script.USER --password-stdin"
-        }
-
+      echo "barev"
     }
 
 
